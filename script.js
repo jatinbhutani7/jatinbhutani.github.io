2edfe2fa9ac0fd5fd4569ede23b6a13336af835c
jatinbhutani7 <jatinbhutani7@gmail.com>
2026-03-26 17:16:29 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,27 @@
 console.log("JS FILE LOADED");
 
 function evaluateAI() {
-  alert("JS is working");
+
+  const problem = document.getElementById("problem").value;
+  const task = document.getElementById("task").value;
+  const workflow = document.getElementById("workflow").value;
+  const outputType = document.getElementById("outputType").value;
+  const error = document.getElementById("error").value;
+  const context = document.getElementById("context").value;
+
+  const outputText = `
+Business Problem: ${problem}
+
+Task: ${task}
+
+Workflow: ${workflow}
+
+Output Type: ${outputType}
+
+Error Tolerance: ${error}
+
+Context Dependency: ${context}
+`;
+
+  document.getElementById("result").innerText = outputText;
 }
